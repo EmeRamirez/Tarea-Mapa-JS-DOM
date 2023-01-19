05409029bba96aeed8f20d69f894875c5ad14a86
Emerson Ramirez <emerson.ramirez.s@gmail.com>
2023-01-18 22:16:45 -0300

    Card modificacion lista

---
+++ js/db.js
@@ -0,0 +1,68 @@
+//Composición de arreglos
+const regiones = [
+    [
+      "img/Morro-de-Arica.jpg",
+      "Morro de Arica", 
+      "El morro de Arica es el sitio más representativo de la región."
+    ],[
+      "img/gigante-de-tarapaca.webp",
+      "El Gigante de Tarapacá",
+      "El gigante de Tarapacá es un sitio histórico de interesante procedencia ubicado en pleno desierto."],
+    [
+      "img/antofapasta.jpg",
+      "Antofapasta",
+      "Especial cuidado con los zombies en esta región."
+    ],[
+      "img/copiapo.jpg",
+      "Copiapó",
+      "Una de las playas más codiciadas del país , Bahía Inglesa"
+    ],[
+      "img/coquimbo.jpg",
+      "Coquimbo",
+      "En las costas de Coquimbo se pueden visitar réplicas de barcos pirata y vivir la experiencia de una batalla en altamar."
+    ],[
+      "img/valparaiso.jpg",
+      "Valparaiso",
+      "El “Queen Victoria Memorial Organ” es un órgano de tubos que data desde finales del siglo XIX. Los días domingos se realizan conciertos gratuitos en la iglesia anglicana St. Paul en Cerro Concepción."
+    ],[
+      "img/rm-stgo.jpg",
+      "Chilezuela",
+      "El infierno de Chile y en el cual se concentra la mayor densidad poblacional del país."
+    ],[
+      "img/rancaguant.png",
+      "Rancagua",
+      "Antiguos mitos dicen que existe un territorio llamado Rancagua, aún no se sabe a ciencia cierta su existencia."
+    ],[
+      "img/talca.jpg",
+      "Talca",
+      "La ciudad más funada de Chile, la del completo mojado y las negligencias clínicas, sin embargo ha dado a luz grandes profesionales como el profe Oscarito."
+    ],[
+      "img/chillan.jpg",
+      "Chillan",
+      "El imperio de las longanizas. Si pasa por ahí, pruébelas."
+    ],[
+      "img/conce.jpg",
+      "Concepción",
+      "Una de las ciudades más pobladas del país, conocida como la capital del sur, alberga un reconocido capital intelectual, universidades y mucho ROCK"
+    ],[
+      "img/temuco.jpeg",
+      "Temuco",
+      "La naturalmente imponente, pero socialmente infame región de la araucanía, si va a este lugar lleve chaleco antibalas y recuerde no fumar, ya que es inflamable."
+    ],[
+      "img/Valdivia.jpg",
+      "Valdivia",
+      "La hermosa ciudad de Valdivia es el sitio ideal para probar cervezas artesanales y emborracharse con una linda vista."
+    ],[
+      "img/puerto.jpeg",
+      "Lago Llanquihue",
+      "A 45 minutos de Puerto Montt, en la ciudad de Osorno, se encuentra el lago Llanquihue. Uno de los lugares más hermosos del país, rodeado de reservas naturales y hermosos parajes en cada uno de sus pueblos aledaños."
+    ],[
+      "img/coyhaique.jpg",
+      "Coyhaique",
+      "En el corazón de la patagonia chilena, se encuentra Coyhaique, una hermosa ciudad con un atractivo tan impresionante como sus bajos grados celsius."
+    ],[
+      "img/williams.jpg",
+      "Punta Arenas/Puerto Williams",
+      "Pucha que hace frío oiga."
+    ]
+  ];
---
+++ js/main.js
@@ -0,0 +1,47 @@
+//Esta función recibe el parametro respectivo a la posición dentro de la imagen y posteriormente modifica los objetos desplegados en el documento.
+function mostrarCiudad(opcion) {
+    $("#contenedor-carta").addClass("oculto");
+  
+    //Se añade una condicional para seleccionar el caso del contenido a mostrar, anidado dentro de un método de intervalo de tiempo para desarrollar un efecto de transición.
+    setTimeout (() => {
+        $("#contenedor-carta").removeClass("oculto");
+        $("#imagen").attr("src", regiones[opcion][0]);
+        $(".card-title").html(regiones[opcion][1]);
+        $(".card-text").html(regiones[opcion][2]);
+    },500);
+}
+
+
+//Funcion para reemplazar imagen
+
+  $('#nvaImagen').on('change',function(event){
+    let foto = event.target.files[0];
+    let fr = new FileReader();
+
+    fr.onload = function(event2) {
+      console.dir(event2);
+      $('#imagen').attr('src',event2.target.result);
+    };
+    fr.readAsDataURL(foto);
+  });
+
+
+//Funcion para modificar textos
+  function cambiarTextos(){
+    // document.querySelector('.card-title').innerHTML = document.getElementById('input-titulo').value;
+    $('.card-title').html($('#input-titulo').val());
+    $('.card-text').html($('#input-texto').val());
+  };
+
+  //Funcion para mostrar o esconder la carta secundaria
+  function alternarCard2(){
+    if ($('#card-modificacion').hasClass('card-oculto') == true) { 
+      $('#card-modificacion').removeClass('card-oculto');
+      $('#boton-ocul').html('Ocultar');
+
+    } else {
+      $('#card-modificacion').addClass('card-oculto');
+      $('#boton-ocul').html('Modificar');
+    };
+      
+  }
